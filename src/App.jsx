b05b35a6c527643e51ab2322690d59ccd7fbb716
patrick-hutchinson <hutchinsonpatrick@icloud.com>
2025-01-03 11:27:06 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import { useEffect, useState, useRef } from "react";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import "./App.css";
+
+import Layout from "./assets/components/Layout/Layout";
+import Home from "./assets/pages/Home/Home";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route index element={<Home />}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/assets/components/Layout/Layout.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { useEffect, useState, useRef } from "react";
+import { Outlet } from "react-router-dom";
+
+import styles from "./Layout.module.css";
+
+export default function Layout() {
+  useEffect(() => {
+    let bg_colours = ["973671", "ECFFE5", "0A4840", "F7DFE6", "333027", "E9F7FF", "824E28"];
+    let font_colours = ["ffb261", "3f76f7", "c4ffb3", "c48d0b", "ae63e1", "678c26", "ff77d0"];
+
+    let randomcolor = Math.floor(Math.random() * bg_colours.length);
+    //console.log(`rgb(${bg_colours[randomcolor]})`);
+    document.querySelector("body").style.backgroundColor = `#${bg_colours[randomcolor]}`;
+    document.querySelector("body").style.color = `#${font_colours[randomcolor]}`;
+    document.querySelector("a").style.color = `#${font_colours[randomcolor]}`;
+    //document.querySelector('a').style.borderBottom = `1px solid #${font_colours[randomcolor]}`;
+
+    function invertHex(hex) {
+      return (Number(`0x1${hex}`) ^ 0xffffff).toString(16).substr(1).toUpperCase();
+    }
+    let invertedFontColor = invertHex(`${font_colours[randomcolor]}`);
+    let invertedBGColor = invertHex(`${bg_colours[randomcolor]}`);
+    console.log(invertedFontColor);
+
+    document.querySelector(":root").style.setProperty("--inverted-font-color", "#" + invertedFontColor);
+    document.querySelector(":root").style.setProperty("--inverted-bg-color", "#" + invertedBGColor);
+    document.querySelector(":root").style.setProperty("--visited-link", "#" + font_colours[randomcolor]);
+
+    if (window.innerWidth < 800) {
+      document.querySelectorAll("img").forEach(function (imgs) {
+        imgs.style.filter = "none";
+      });
+    }
+
+    //black and white photo effect
+    document.querySelectorAll("img").forEach(function (imgs) {
+      imgs.addEventListener("mouseenter", function () {
+        imgs.style.filter = "none(1)";
+        imgs.setAttribute("style", "-webkit-filter:none");
+      });
+
+      imgs.addEventListener("mouseleave", function () {
+        imgs.style.filter = "invert(1)";
+        imgs.setAttribute("style", "-webkit-filter:invert(1)");
+      });
+    });
+
+    console.log(window.innerWidth);
+  }, []);
+
+  return (
+    <>
+      <Outlet />
+    </>
+  );
+}
